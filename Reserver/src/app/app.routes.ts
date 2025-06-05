865d8b0ad1137da62344e139face8f3279f94dd2
landeryt <mcraft160105@gmail.com>
2025-06-05 20:00:29 +0200

    Added hyperlink to the two components

--- Reserver/src/app/app.routes.ts
+++ Reserver/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ReservationComponent } from './reservation/reservation.component';
+import { MyReservationsComponent } from './my-reservations/my-reservations.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: "reservation", component: ReservationComponent },
+    { path: "myreservations", component: MyReservationsComponent }
+];
